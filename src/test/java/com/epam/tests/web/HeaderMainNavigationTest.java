46460d7b01ebbfe825f500d576c81f251697ed4a
Linh Tran <tranthilinh196@gmail.com>
2023-06-05 11:09:37 +0700

    update

--- src/test/java/com/epam/tests/web/HeaderMainNavigationTest.java
+++ src/test/java/com/epam/tests/web/HeaderMainNavigationTest.java
@@ -8,44 +8,44 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 @Tag("linhtest")
 public class HeaderMainNavigationTest extends PlaywrightWebRunner{
-//    @Test
-//    @Description("Test case 584")
-//    public void verifyLabelDisplayed() {
-//        homePage.navigate();
-//        headerPage.verifyCareerLabelDisplayed();
-//        headerPage.verifyAboutsLabelDisplayed();
-//        headerPage.verifyIndustriesLabelDisplayed();
-//        headerPage.verifyInsightsLabelDisplayed();
-//        headerPage.verifyServicesLabelDisplayed();
-//    }
+    @Test
+    @Description("Test case 584")
+    public void verifyLabelDisplayed() {
+        homePage.navigate();
+        headerPage.verifyCareerLabelDisplayed();
+        headerPage.verifyAboutsLabelDisplayed();
+        headerPage.verifyIndustriesLabelDisplayed();
+        headerPage.verifyInsightsLabelDisplayed();
+        headerPage.verifyServicesLabelDisplayed();
+    }
 
-//    @Test
-//    @Description("Test case 586 + 580")
-//    public void verifyMainNavigationAlwaysAvailable() {
-//        homePage.navigate();
-//        headerPage.ClickOnServicesItem();
-//        headerPage.verifyAboutsLabelDisplayed();
-//        headerPage.verifyIndustriesLabelDisplayed();
-//        headerPage.verifyInsightsLabelDisplayed();
-//        headerPage.verifyServicesLabelDisplayed();
-//    }
-//    @Test
-//    @Description("Test case 542")
-//    public void verifyMainNavigationDisplay(){
-//        homePage.navigate();
-//        headerPage.verifyMainNavigationDisplayedTop();
-//    }
-//
-//    @Test
-//    @Description("Test case 585")
-//    public void verifyColorChangeOfMainNavigation(){
-//        homePage.navigate();
-////        headerPage.verifyColorChangeOfIndustries();
-//        headerPage.verifyColorChangeOfAbout();
-//        headerPage.verifyColorChangeOfCareer();
-//        headerPage.verifyColorChangeOfInsight();
-//        headerPage.verifyColorChangeOfServices();
-//    }
+    @Test
+    @Description("Test case 586 + 580")
+    public void verifyMainNavigationAlwaysAvailable() {
+        homePage.navigate();
+        headerPage.ClickOnServicesItem();
+        headerPage.verifyAboutsLabelDisplayed();
+        headerPage.verifyIndustriesLabelDisplayed();
+        headerPage.verifyInsightsLabelDisplayed();
+        headerPage.verifyServicesLabelDisplayed();
+    }
+    @Test
+    @Description("Test case 542")
+    public void verifyMainNavigationDisplay(){
+        homePage.navigate();
+        headerPage.verifyMainNavigationDisplayedTop();
+    }
+
+    @Test
+    @Description("Test case 585")
+    public void verifyColorChangeOfMainNavigation(){
+        homePage.navigate();
+//        headerPage.verifyColorChangeOfIndustries();
+        headerPage.verifyColorChangeOfAbout();
+        headerPage.verifyColorChangeOfCareer();
+        headerPage.verifyColorChangeOfInsight();
+        headerPage.verifyColorChangeOfServices();
+    }
     @ParameterizedTest
     @ValueSource(strings = { "Services", "Insights", "About", "Careers"})
     @Description("Test case 577")
