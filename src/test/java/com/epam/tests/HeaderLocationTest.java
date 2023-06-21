16cc72480fa68948eef83030a50fa0d97b887028
Khiem Cao <Khiem_Cao@epam.com>
2023-06-21 10:35:25 +0700

    feat: run new tests

--- src/test/java/com/epam/tests/HeaderLocationTest.java
+++ src/test/java/com/epam/tests/HeaderLocationTest.java
@@ -4,6 +4,8 @@ import com.epam.runners.PlaywrightRunner;
 import core.Configuration;
 import core.enums.MENU;
 import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -26,7 +28,7 @@ public class HeaderLocationTest extends PlaywrightRunner {
         return JsonReader.load("testData/LocationData.json");
     }
 
-    @BeforeMethod(alwaysRun=true)
+    @BeforeMethod(alwaysRun = true)
     public void setConditions() {
         homePage.navigate();
         if (Boolean.parseBoolean(Configuration.get().getProperty("isMobile"))) {
@@ -34,30 +36,37 @@ public class HeaderLocationTest extends PlaywrightRunner {
         }
     }
 
-    @Test
-    @Description("Test case 571")
+    @Story("525 - Location button")
+    @Test(groups = {"525"}, description = "[Location] Verify default text on Location button")
+    @Description("Test cases: 571")
     public void verifyLocationDefaultText() {
         headerPage.clickLocationMenu();
         headerPage.verifyLocationDefaultTextDisplayed();
     }
 
-    @Test (groups = { "smoke" })
-    @Description("Test case 573")
+    @Feature("Smoke")
+    @Story("525 - Location button")
+    @Test(groups = {"smoke", "525"}, description = "[Location] Verify all required location are listed out")
+    @Description("Test cases: 573")
     public void verifyAllLocationAreDisplayed() {
         headerPage.clickLocationMenu();
         headerPage.verifyLocationMenuDisplayed();
     }
 
-    @Test(dataProvider = "locationData")
-    @Description("Test case 575 + 579")
+    @Story("525 - Location button")
+    @Test(groups = {"525"}, description = "[Location] Verify the corresponding navigation link",
+            dataProvider = "locationData")
+    @Description("Test cases: 575, 579")
     public void verifySwitchLocation(String location, String title, String url) {
         headerPage.clickLocationMenu();
         headerPage.clickLocationOption(location);
         headerPage.verifySwitchLocationSuccessful(title, url);
     }
 
-    @Test(dataProvider = "navOptions")
-    @Description("Test case 577")
+    @Story("525 - Location button")
+    @Test(groups = {"525"}, description = "[Location] Verify the display of Location button",
+            dataProvider = "navOptions")
+    @Description("Test cases: 577")
     public void verifyLocationDisplayedOnAnyPages(String mainNavigationName) {
         headerPage.clickMainNavigationOption(mainNavigationName);
         if (Boolean.parseBoolean(Configuration.get().getProperty("isMobile"))) {
